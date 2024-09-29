812e544e33d1b6b59ef51d01b2e8f38099e646be
Emma Bjørg <emso0005@stud.kea.dk>
2024-09-29 12:34:27 +0200

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: [],
   theme: {
+    colors: {
+      'brightsun':'#F9D142',
+      'greenwhite':'#E9E9E9',
+      'deepaqua':'#008080',
+      'balticsea':'#292826',
+    },
     extend: {},
   },
   plugins: [],
