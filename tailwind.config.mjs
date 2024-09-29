5550ac5287f69f4e89e85aa77e16f406482f53c2
Emma Bjørg <emso0005@stud.kea.dk>
2024-09-29 21:03:31 +0200

    portræt....

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,7 +11,7 @@ export default {
 		extend: {
             fontFamily: {
                 'fragment-mono': ['Fragment Mono', 'monospace'],
-            }
+            },
         },
 	},
 	plugins: [
